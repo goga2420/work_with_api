0ae8546bff83922531dbca129ed0ea569cd2e6e4
goga2420 <58983503+goga2420@users.noreply.github.com>
2020-04-29 18:25:11 +0300

    Добавлены ссылки на репозитории

--- 17_1.py
+++ 17_1.py
@@ -11,11 +11,6 @@ print("Total repositories:", response_dict['total_count'])
 
 repo_dicts = response_dict['items']
 
-names, stars = [], []
-for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-
 my_style = LS('#333366', base_style=LCS)
 my_style.tittle_font_size = 24
 my_style.label_font_size = 14
@@ -27,9 +22,21 @@ my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
 
+
 chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred JavaScript Projects on GitHub'
-chart.x_labels = names
 
-chart.add('', stars)
+chart_data = []
+names = []
+for repo_dict in repo_dicts:
+    repo_chart_data = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['name'],
+        'xlink': repo_dict['html_url']
+    }
+    chart_data.append(repo_chart_data)
+    names.append(repo_dict['name'])
+
+chart.add('Some value', chart_data)
+chart.x_labels = names
 chart.render_to_file('java_repos.svg')
